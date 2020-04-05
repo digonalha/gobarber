a8ef80037e2593f32f015cbeb643879be6b14421
digonalha <digonalha@gmail.com>
2020-04-05 20:36:55 -0300

    criando validação da autenticação com jwt

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
 routes.post('/sessions', SessionController.store);
+
+// o middleware só vai funcionar nas rotas abaixo dele
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
