646276d8595cb2b7bebe8a21c2a78b53ab103893
digonalha <digonalha@gmail.com>
2020-04-06 00:31:24 -0300

    validação de schema com o yup

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,20 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    // schema para validação dos campos
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    // verifico se os campos que vieram do body passam na validação do schema
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: 'Validation fails' });
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
