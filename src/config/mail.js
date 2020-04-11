7f4f1655c7b08bfa4b716f126b4f80145a4962d9
digonalha <digonalha@gmail.com>
2020-04-11 01:26:59 -0300

    criando viewes e classes para envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '9a62101017fa73',
+    pass: '8c3c31c727f331',
+  },
+  default: {
+    from: 'noreply <noreply@gobarber.com>',
+  },
+};
